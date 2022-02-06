ea8b401d5a5655be3b9b4cdb419026ce8f21e003
Denis <denis.tanjingyu@hotmail.sg>
2022-02-06 22:02:29 +0800

    Add sql file

---
+++ 184. Department Highest Salary.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+WITH ranked_table AS (
+    SELECT Department.name AS Department, 
+           Employee.name AS Employee, 
+           Employee.salary AS Salary,
+           RANK() OVER(PARTITION BY Employee.departmentId ORDER BY Employee.salary DESC) AS      salary_rank
+    FROM Employee
+    INNER JOIN Department
+    ON Employee.departmentId = Department.id
+)
+
+SELECT Department, 
+       Employee, 
+       Salary
+FROM ranked_table
+WHERE salary_rank = 1
